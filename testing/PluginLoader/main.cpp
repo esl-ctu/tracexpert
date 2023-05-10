71b9d5858026e655c4d14844bafdfdb5688b1ab3
tomaspre.e490 <tomas.preucil@fit.cvut.cz>
2023-05-10 17:54:46 +0200

    Tune the plugin loader to start testing newae plugin

--- testing/PluginLoader/main.cpp
+++ testing/PluginLoader/main.cpp
@@ -1,23 +1,60 @@
 #include <QCoreApplication>
 #include <QPluginLoader>
 #include <QDir>
+#include <QtDebug>
+#include <QFile>
+#include <QTextStream>
+
 #include "tplugin.h"
+
+void myMessageHandler(QtMsgType type, const QMessageLogContext &, const QString & msg)
+{
+    QString txt;
+    switch (type) {
+    case QtDebugMsg:
+        txt = QString("Debug: %1").arg(msg);
+        break;
+    case QtWarningMsg:
+        txt = QString("Warning: %1").arg(msg);
+        break;
+    case QtCriticalMsg:
+        txt = QString("Critical: %1").arg(msg);
+        break;
+    case QtFatalMsg:
+        txt = QString("Fatal: %1").arg(msg);
+        //abort();
+        break;
+    default:
+        txt = QString("%1").arg(msg);
+    }
+    QFile outFile("log");
+    outFile.open(QIODevice::WriteOnly | QIODevice::Append);
+    QTextStream ts(&outFile);
+    ts << txt << Qt::endl;
+}
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+    qInstallMessageHandler(myMessageHandler);
+    qWarning("Start program");
 
     QTextStream stream(stdout);
+    stream << "* Started plugin loader" << Qt::endl;
 
     QDir pluginsDir(QCoreApplication::instance()->applicationDirPath());
 
     foreach (QString fileName, pluginsDir.entryList(QDir::Files)) {
         QPluginLoader pluginLoader(pluginsDir.absoluteFilePath(fileName));
+        stream << "* FN: " << fileName;
 
         QObject *plugin = pluginLoader.instance();
-        if (plugin) {
-            TPlugin * serialPorts = qobject_cast<TPlugin *>(plugin);
-            if (serialPorts){
+        if (!plugin) {
+            stream << " - ignoring" <<  Qt::endl;
+        } else {
+            stream << Qt::endl;
+            TPlugin * aPlugin = qobject_cast<TPlugin *>(plugin);
+            if (aPlugin){
 
                 QString pluginId = fileName;
                 if(pluginId.startsWith("lib")) pluginId.remove(0, 3);
@@ -25,49 +62,66 @@ int main(int argc, char *argv[])
                 if(pluginId.endsWith(".so")) pluginId.chop(3);
                 if(pluginId.endsWith(".dll")) pluginId.chop(4);
 
-                stream << "* Plug-in ID: '" << pluginId << "', name: '" << serialPorts->getPluginName() << "'\n";
-                stream << "    Description: '" << serialPorts->getPluginInfo() << "'\n\n";
+                stream << "* Plug-in ID: '" << pluginId << "', name: '" << aPlugin->getPluginName() << "'\n";
+                stream << "    Description: '" << aPlugin->getPluginInfo() << "'\n\n";
                 stream.flush();
 
-                //TConfigParam test = serialPorts->getPreInitParams();
-               // test.setValue("69, 420");
-               // serialPorts->setPreInitParams(test);
+                if (pluginId.contains("serial")){
+                    stream << "* Starting tests of the serial plugin" << Qt::endl;
 
-                serialPorts->init();
+                    TPlugin * serialPorts = aPlugin;
 
-                QList<TIODevice *> devices = serialPorts->getIODevices();
+                    //TConfigParam test = serialPorts->getPreInitParams();
+                   // test.setValue("69, 420");
+                   // serialPorts->setPreInitParams(test);
 
-                for(int i = 0; i < devices.size(); i++){
+                    serialPorts->init();
 
-                    stream << devices[i]->getIODeviceName() << "\n";
-                    stream << devices[i]->getIODeviceInfo() << "\n";
+                    QList<TIODevice *> devices = serialPorts->getIODevices();
+
+                    for(int i = 0; i < devices.size(); i++){
+
+                        stream << devices[i]->getIODeviceName() << "\n";
+                        stream << devices[i]->getIODeviceInfo() << "\n";
+                        stream.flush();
+
+                    }
+
+                    bool iok = false;
+
+                    devices[0]->init(&iok);
+                    //stream << iok << "\n";
+
+                    TConfigParam params = devices[0]->getPostInitParams();
+                    params.getSubParamByName("Baudrate")->setValue("Custom");
+                    params.getSubParamByName("Custom baudrate")->setValue("256000");
+                    devices[0]->setPostInitParams(params);
+
+                    params = devices[0]->getPostInitParams();
+                    stream << "Baudrate:" << params.getSubParamByName("Baudrate")->getValue() << "\n";
                     stream.flush();
 
+                    uint8_t data[10] = {10, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+                    devices[0]->writeData(data, 10);
+                    size_t read = devices[0]->readData(data, 10);
+                    if(read == 0) {
+                        stream << "No data read" << "\n";
+                    } else {
+                        data[read-1] = 0;
+                        stream << (char *) data << "\n";
+                    }
                 }
 
-                bool iok = false;
+                if (pluginId.contains("newae")){
+                    stream << "* Starting tests of the NewAE plugin" << Qt::endl;
 
-                devices[0]->init(&iok);
-                //stream << iok << "\n";
-
-                TConfigParam params = devices[0]->getPostInitParams();
-                params.getSubParamByName("Baudrate")->setValue("Custom");
-                params.getSubParamByName("Custom baudrate")->setValue("256000");
-                devices[0]->setPostInitParams(params);
-
-                params = devices[0]->getPostInitParams();
-                stream << "Baudrate:" << params.getSubParamByName("Baudrate")->getValue() << "\n";
-                stream.flush();
-
-                uint8_t data[10] = {10, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-
-                devices[0]->writeData(data, 10);
-                size_t read = devices[0]->readData(data, 10);
-                if(read == 0) {
-                    stream << "No data read" << "\n";
-                } else {
-                    data[read-1] = 0;
-                    stream << (char *) data << "\n";
+                    TPlugin * newaePlug = aPlugin;
+                    bool ok = false;
+                    newaePlug->init(&ok);
+                    if (!ok) {
+                        stream << "** Load failed" << Qt::endl;
+                    }
                 }
 
             }
