8fc8d3932e923e6cb88f76d2e1a17186fbaa3d7a
David Pokorný <davidpoky@gmail.com>
2023-05-10 16:15:37 +0200

    test of PluginLoader

--- testing/PluginLoader/main.cpp
+++ testing/PluginLoader/main.cpp
@@ -1,7 +1,48 @@
+#include <chrono>
+#include <thread>
 #include <QCoreApplication>
 #include <QPluginLoader>
 #include <QDir>
 #include "tplugin.h"
+#include <ctime>
+
+
+void ReadAndWriteBack(TIODevice *device, QTextStream &stream, size_t len){
+    // Add the read and write operations for the second device here
+    uint8_t data[100000+1];
+
+    if(len > 100000){
+        stream << "len too big\n";
+        stream.flush();
+        return;
+    }
+
+    stream << "Reading..." << "\n";
+    stream.flush();
+    size_t read = device->readData(data, len);
+    if(read == 0) {
+        stream << "No data read" << "\n";
+    } else {
+        data[read] = 0;
+        stream << (char *) data << "\n";
+        /*if(data[0] != cnt){
+            stream << "not sync: current " << cnt << ", recieved: " << data[0] << "\n";
+        }*/
+    }
+
+    stream << "Writing..." << "\n";
+    stream.flush();
+    size_t writeLen;
+    if(read == 0){
+        uint8_t noDataText[] = "no data  \0";
+        writeLen = device->writeData(noDataText, sizeof(noDataText));
+    }else{
+        writeLen = device->writeData(data, read);
+    }
+    stream << "Writed " << writeLen << " bytes\n";
+    stream.flush();
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -11,13 +52,17 @@ int main(int argc, char *argv[])
 
     QDir pluginsDir(QCoreApplication::instance()->applicationDirPath());
 
+    srand(time(0));
+
     foreach (QString fileName, pluginsDir.entryList(QDir::Files)) {
         QPluginLoader pluginLoader(pluginsDir.absoluteFilePath(fileName));
 
         QObject *plugin = pluginLoader.instance();
         if (plugin) {
             TPlugin * serialPorts = qobject_cast<TPlugin *>(plugin);
+
             if (serialPorts){
+
 
                 QString pluginId = fileName;
                 if(pluginId.startsWith("lib")) pluginId.remove(0, 3);
@@ -37,40 +82,141 @@ int main(int argc, char *argv[])
 
                 QList<TIODevice *> devices = serialPorts->getIODevices();
 
+                QString portName = "COM3";
+                TIODevice *selectedPort = nullptr;
                 for(int i = 0; i < devices.size(); i++){
-
-                    stream << devices[i]->getIODeviceName() << "\n";
-                    stream << devices[i]->getIODeviceInfo() << "\n";
+                    stream << "port name: " << devices[i]->getIODeviceName() << "\n";
+                    stream << "port info: " << devices[i]->getIODeviceInfo() << "\n";
                     stream.flush();
 
+                    if(devices[i]->getIODeviceName() == portName){
+                        selectedPort = devices[i];
+                        //continue;
+                    }
                 }
 
-                bool iok = false;
 
-                devices[0]->init(&iok);
-                //stream << iok << "\n";
+                if(!selectedPort){
+                    stream << "device not found" << "\n";
+                    stream.flush();
+                    continue;
+                }
 
-                TConfigParam params = devices[0]->getPostInitParams();
-                params.getSubParamByName("Baudrate")->setValue("Custom");
-                params.getSubParamByName("Custom baudrate")->setValue("256000");
-                devices[0]->setPostInitParams(params);
-
-                params = devices[0]->getPostInitParams();
-                stream << "Baudrate:" << params.getSubParamByName("Baudrate")->getValue() << "\n";
+                stream << "selected: port name: " << selectedPort->getIODeviceName() << "\n";
+                stream << "selected: port info: " << selectedPort->getIODeviceInfo() << "\n";
                 stream.flush();
 
-                uint8_t data[10] = {10, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-
-                devices[0]->writeData(data, 10);
-                size_t read = devices[0]->readData(data, 10);
-                if(read == 0) {
-                    stream << "No data read" << "\n";
-                } else {
-                    data[read-1] = 0;
-                    stream << (char *) data << "\n";
+                bool iok = false;
+                selectedPort->init(&iok);
+                if(!iok){
+                    stream << "device cannot be inicializated" << "\n";
+                    stream.flush();
+                    break;
                 }
 
+                std::this_thread::sleep_for(std::chrono::seconds(3));
+
+                std::vector<int> baudrates = {1200, 2400, 4800, 9600, 19200, 38400, 57600, 115200};
+                std::vector<int> bytesizes = {8};//{5, 6, 7, 8};
+                std::vector<QString> parities = {"None", "Even", "Odd", "Mark", "Space"};
+                std::vector<QString> stopbits = {"One", /*"One and half",*/ "Two"};
+                std::vector<QString> flow_controls = {"None", "Hardware (RTS/CTS)", "Software (XON/XOFF)"};
+
+                //Testing one by one
+                if(false){
+                    int cnt = 0;
+
+                    for (int baudrate : baudrates) {
+                        for (int bytesize : bytesizes) {
+                            for (const QString& parity : parities) {
+                                for (const QString& stopbit : stopbits) {
+                                    for (const QString& flow_control : flow_controls) {
+                                        cnt++;
+                                        //if(cnt<9) continue;
+                                        //if(flow_control == "RTS/CTS") continue;
+
+                                        stream << cnt << ") Trying combination: baudrate=" << baudrate << ", bytesize=" << bytesize
+                                                  << ", parity=" << parity << ", stopbits=" << stopbit
+                                                  << ", flow_control=" << flow_control << "\n";
+                                        stream.flush();
+
+                                        TConfigParam params = selectedPort->getPostInitParams();
+                                        params.getSubParamByName("Baudrate")->setValue(baudrate);
+                                        //params.getSubParamByName("Data bits")->setValue(bytesize);
+                                        params.getSubParamByName("Parity")->setValue(parity);
+                                        params.getSubParamByName("Stop bits")->setValue(stopbit);
+                                        params.getSubParamByName("Flow control")->setValue(flow_control);
+                                        params.getSubParamByName("Read timeout (ms)")->setValue("5000");
+                                        selectedPort->setPostInitParams(params);
+
+
+                                        ReadAndWriteBack(selectedPort, stream, 100);
+
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                //Manual testing
+                if(true){
+                    // Randomly select parameters
+                    int baudrate = 19200;
+                    int bytesize = 8;
+                    QString parity = "Even";
+                    QString stopbit = "One and half";
+                    QString flow_control = "None";
+
+                    // Print the selected parameters
+                    stream << "Trying combination: baudrate=" << baudrate << ", bytesize=" << bytesize
+                           << ", parity=" << parity << ", stopbits=" << stopbit
+                           << ", flow_control=" << flow_control << "\n";
+                    stream.flush();
+
+                    TConfigParam params = selectedPort->getPostInitParams();
+                    params.getSubParamByName("Baudrate")->setValue(baudrate);
+                    //params.getSubParamByName("Data bits")->setValue(bytesize);
+                    params.getSubParamByName("Parity")->setValue(parity);
+                    params.getSubParamByName("Stop bits")->setValue(stopbit);
+                    params.getSubParamByName("Flow control")->setValue(flow_control);
+                    params.getSubParamByName("Read timeout (ms)")->setValue("100000");
+                    selectedPort->setPostInitParams(params);
+
+                    ReadAndWriteBack(selectedPort, stream, 10000);
+                }
+
+                //Manual testing - random mode
+                if(false){
+
+                        // Randomly select parameters
+                        int baudrate = baudrates[rand() % baudrates.size()];
+                        int bytesize = bytesizes[rand() % bytesizes.size()];
+                        QString parity = parities[rand() % parities.size()];
+                        QString stopbit = stopbits[rand() % stopbits.size()];
+                        QString flow_control = flow_controls[rand() % flow_controls.size()];
+                    for(int i = 0; i<10; i++){
+                        // Print the selected parameters
+                        stream << "Trying combination: baudrate=" << baudrate << ", bytesize=" << bytesize
+                               << ", parity=" << parity << ", stopbits=" << stopbit
+                               << ", flow_control=" << flow_control << "\n";
+                        stream.flush();
+
+                        TConfigParam params = selectedPort->getPostInitParams();
+                        params.getSubParamByName("Baudrate")->setValue(baudrate);
+                        //params.getSubParamByName("Data bits")->setValue(bytesize);
+                        params.getSubParamByName("Parity")->setValue(parity);
+                        params.getSubParamByName("Stop bits")->setValue(stopbit);
+                        params.getSubParamByName("Flow control")->setValue(flow_control);
+                        params.getSubParamByName("Read timeout (ms)")->setValue("100000");
+                        selectedPort->setPostInitParams(params);
+
+                        ReadAndWriteBack(selectedPort, stream, 5);
+                    }
+                }
             }
+
+            stream.flush();
         }
         pluginLoader.unload();
     }
