2208efaf6d91a836de3e5ab88ca7dcc0171aa154
Petr Socha <petr.socha@fit.cvut.cz>
2023-07-28 14:52:47 +0200

    EmptyPlugin (in plugins/) and EmptyPluginLoader (in testing/), as a template for new plugins

---
+++ plugins/emptyplugin/temptyplugin.h
@@ -0,0 +1,56 @@
+#ifndef TEMPTYPLUGIN_H
+#define TEMPTYPLUGIN_H
+
+#include "TEmptyPlugin_global.h"
+#include "tplugin.h"
+
+
+class TEMPTYPLUGIN_EXPORT TEmptyPlugin : public QObject, TPlugin
+{
+
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID "org.cvut.fit.TraceXpert.PluginInterface/1.0" FILE "temptyplugin.json")
+    Q_INTERFACES(TPlugin)
+
+public:
+    TEmptyPlugin();
+    virtual ~TEmptyPlugin() override;
+
+    /// Plugin name
+    virtual QString getPluginName() const override;
+    /// Plugin info
+    virtual QString getPluginInfo() const override;
+
+    /// Get the current pre-initialization parameters
+    virtual TConfigParam getPreInitParams() const override;
+    /// Set the pre-initialization parameters, returns the current params after set
+    virtual TConfigParam setPreInitParams(TConfigParam params) override;
+
+    /// Initialize the plugin
+    virtual void init(bool *ok = nullptr) override;
+    /// Deinitialize the plugin
+    virtual void deInit(bool *ok = nullptr) override;
+
+    /// Get the current post-initialization parameters
+    virtual TConfigParam getPostInitParams() const override;
+    /// Set the post-initialization parameters, returns the current params after set
+    virtual TConfigParam setPostInitParams(TConfigParam params) override;
+
+    virtual TIODevice * addIODevice(QString name, QString info, bool *ok = nullptr) override;
+    virtual TScope * addScope(QString name, QString info, bool *ok = nullptr) override;
+
+    virtual bool canAddIODevice() override;
+    virtual bool canAddScope() override;
+
+    /// Get available IO devices, available only after init()
+    virtual QList<TIODevice *> getIODevices() override;
+    /// Get available Scopes, available only after init()
+    virtual QList<TScope *> getScopes() override;
+
+protected:
+
+
+
+};
+
+#endif // TEMPTYPLUGIN_H
