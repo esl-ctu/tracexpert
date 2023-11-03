995ace722f5c68340c791941184abc791358465b
David Pokorný <davidpoky@gmail.com>
2023-11-03 17:06:32 +0100

    Test of TProtocol, TMessage and TMessagePart

--- testing/TProtocol/main.cpp
+++ testing/TProtocol/main.cpp
@@ -2,17 +2,33 @@
 #include <QPluginLoader>
 #include <QDir>
 #include "tprotocol.h"
+#include <iostream>
+#include <random>
+#include <string>
+#include <QVariant>
+#include <QRandomGenerator>
 using namespace Qt;
+
+
+
+bool is_big_endian(void)
+{
+    union {
+        uint32_t i;
+        char c[4];
+    } bint = {0x01020304};
+
+    return bint.c[0] == 1;
+}
+
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
     QTextStream stream(stdout);
-
     bool ok;
 
-    /*
+/*
     // test command message generation
     TMessage c1("Encipher command", "This command instructs device to encipher supplied payload.", false);
 
@@ -56,9 +72,9 @@ int main(int argc, char *argv[])
         stream << hex << (unsigned char)buffer[i] << " ";
     }
     stream << endl;
-    */
+*/
 
-    /*
+/*
     // test static length response message matching
     TProtocol p1("Protocol 1", "empty description");
 
@@ -108,8 +124,9 @@ int main(int argc, char *argv[])
 
     }
 
-    */
+*/
 
+/*
     // test dynamic length response message matching
     TProtocol p1("Protocol 1", "empty description");
 
@@ -123,7 +140,7 @@ int main(int argc, char *argv[])
     stream << (ok ? "Ok" : "Error") << endl;
 
     TMessagePart lengthspec("Length specifier", "", TMessagePart::TType::TByte, true);
-    lengthspec.setByteValue(5, &ok);
+    lengthspec.setByteValue(60, &ok);
     stream << (ok ? "Ok" : "Error") << endl;
 
     c1.addMessagePart(lengthspec, &ok);
@@ -165,5 +182,219 @@ int main(int argc, char *argv[])
         stream << endl;
 
     }
+*/
+
+    TProtocol p1("Protocol 1", "empty description");
+
+    {
+        TMessage m1("Vstup", "vstup", false);
+
+        TMessagePart header("String na zacatku", "", TMessagePart::TType::TString, false, {}, true, 3);
+        header.setValue("abc", &ok);
+        stream << (ok ? "Ok" : "Error") << endl;
+        m1.addMessagePart(header, &ok);
+        stream << (ok ? "Ok" : "Error") << endl;
+
+        TMessagePart constant("just constant value", "", TMessagePart::TType::TByte, false);
+        constant.setByteValue(0x58, &ok);
+        stream << (ok ? "Ok" : "Error") << endl;
+        m1.addMessagePart(constant, &ok);
+        stream << (ok ? "Ok" : "Error") << endl;
+
+        TMessagePart len1("len in int", "", TMessagePart::TType::TChar, false);
+        len1.setValue((signed char)3, &ok);
+        stream << (ok ? "Ok" : "Error") << endl;
+        m1.addMessagePart(len1, &ok);
+        stream << (ok ? "Ok" : "Error") << endl;
+
+        char databuff[3] = {};
+        QByteArray byteArray(databuff, sizeof(databuff));
+        TMessagePart data("mydata", "", TMessagePart::TType::TByteArray, false, {}, true, sizeof(databuff));
+        data.setValue(byteArray, &ok);
+        stream << (ok ? "Ok" : "Error") << endl;
+        m1.addMessagePart(data, &ok);
+        stream << (ok ? "Ok" : "Error") << endl;
+
+        TMessagePart intlen("lenghtbased on uint", "", TMessagePart::TType::TUInt, false);
+        intlen.setValue(10, &ok);
+        stream << (ok ? "Ok" : "Error") << endl;
+        m1.addMessagePart(intlen, &ok);
+        stream << (ok ? "Ok" : "Error") << endl;
+
+        TMessagePart data2("mydata2", "", TMessagePart::TType::TString, false, {}, true, 10);
+        data2.setValue("String 890", &ok);
+        stream << (ok ? "Ok" : "Error") << endl;
+        m1.addMessagePart(data2, &ok);
+        stream << (ok ? "Ok" : "Error") << endl;
+
+        m1.validateMessage();
+        stream << "state m1: " << (int)m1.getState() << endl;
+
+        p1.addMessage(m1, &ok);
+        stream << (ok ? "Ok" : "Error") << endl;
+    }
+
+
+
+    {
+        TMessage m2("Vystup", "", true);
+
+        TMessagePart header("String na zacatku", "", TMessagePart::TType::TString, false, {}, true, 3);
+        header.setValue("abc", &ok);
+        stream << (ok ? "Ok" : "Error") << endl;
+        m2.addMessagePart(header, &ok);
+        stream << (ok ? "Ok" : "Error") << endl;
+
+        TMessagePart constant("just constant value", "", TMessagePart::TType::TByte, false);
+        constant.setByteValue(0x58, &ok);
+        stream << (ok ? "Ok" : "Error") << endl;
+        m2.addMessagePart(constant, &ok);
+        stream << (ok ? "Ok" : "Error") << endl;
+
+        TMessagePart len1("len in int", "", TMessagePart::TType::TChar, true, {}, true, 0, false);
+        //len1.setValue((signed char)3, &ok);
+        //stream << (ok ? "Ok" : "Error") << endl;
+        m2.addMessagePart(len1, &ok);
+        stream << (ok ? "Ok" : "Error") << endl;
+
+
+        TMessagePart data("mydata", "", TMessagePart::TType::TByteArray, true, {}, false, 2);
+        //data.setValue({}, &ok);
+        //stream << (ok ? "Ok" : "Error") << endl;
+        m2.addMessagePart(data, &ok);
+        stream << (ok ? "Ok" : "Error") << endl;
+
+        TMessagePart intlen("lenghtbased on uint", "", TMessagePart::TType::TUInt, true, {}, true, 0, false);
+        //intlen.setValue(0, &ok);
+        //stream << (ok ? "Ok" : "Error") << endl;
+        m2.addMessagePart(intlen, &ok);
+        stream << (ok ? "Ok" : "Error") << endl;
+
+        TMessagePart data2("mydata2", "", TMessagePart::TType::TString, true, {}, false, 4);
+        //data2.setValue("String 890", &ok);
+        //stream << (ok ? "Ok" : "Error") << endl;
+        m2.addMessagePart(data2, &ok);
+        stream << (ok ? "Ok" : "Error") << endl;
+
+        m2.validateMessage();
+        stream << (int) m2.getState() << endl;
+        stream << "delka m2: " << m2.getLength() << endl;
+
+        p1.addMessage(m2, &ok);
+        stream << (ok ? "Ok" : "Error") << endl;
+    }
+
+    {
+        auto m1 = p1.getMessageByName("Vstup");
+        if(m1 == nullptr) stream << "vstup nenalezen" << endl;
+        else{
+            qsizetype m1len = m1->getLength();
+            stream << "state m1: " << m1->getStateMessage() << endl;
+            stream << "delka m1: " << m1len << endl;
+
+            uint8_t buffer[100];
+            qsizetype size = m1->getData(buffer, 100);
+            stream << "Message vstup:" << endl;
+            for(int i = 0; i < size; i++) {
+                stream << hex << (unsigned char)buffer[i] << " ";
+            }
+            stream << endl;
+
+
+
+
+            TMessage * matchedMessage = p1.tryMatchResponse(buffer, size);
+
+            if(matchedMessage != nullptr) {
+
+                uint8_t buffer[100];
+
+                stream << matchedMessage->getData(buffer, 100) << endl;
+
+                for(int i = 0; i < matchedMessage->getLength(); i++) {
+                    stream << hex << (unsigned char)buffer[i] << " ";
+                }
+                stream << endl;
+
+
+                {
+                    //získání dat ze zprávy: "mydata2"
+                    QString MessagePartName = "mydata2";
+                    TMessagePart * data = matchedMessage->getMessagePartByName(MessagePartName);
+                    stream << "data from:" << MessagePartName << endl;
+                    data->getData(buffer, 100);
+                    auto len = matchedMessage->getMessagePartLengthByName(MessagePartName);
+                    for(int i = 0; i < len ; i++) {
+                        stream << hex << buffer[i] << " ";
+                    }
+                    stream << endl;
+                }
+
+                {
+                    //získání dat ze zprávy: "len in int"
+                    QString MessagePartName = "len in int";
+                    TMessagePart * data = matchedMessage->getMessagePartByName(MessagePartName);
+                    stream << "data from:" << MessagePartName << endl;
+                    data->getData(buffer, 100);
+                    auto len = matchedMessage->getMessagePartLengthByName(MessagePartName);
+                    for(int i = 0; i < len ; i++) {
+                        stream << hex << buffer[i] << " ";
+                    }
+                    stream << endl;
+                }
+
+
+            }else stream << "not found match" << endl;
+        }
+    }
+
+    //kontrola endianity
+    {
+
+        stream << "my pc is " << (is_big_endian() ? "big-endian" : "little-endian") << endl; // v tomto testu by na tom nemelo zalezet
+
+        TMessage m("Response endian", "", true);
+
+        TMessagePart intlen("lenghtbased on uint", "", TMessagePart::TType::TUInt, true, {}, true, 0, true); //vstup je little-endian
+
+        m.addMessagePart(intlen, &ok);
+        stream << (ok ? "Ok" : "Error") << endl;
+
+        TMessagePart data2("mydata2", "", TMessagePart::TType::TString, true, {}, false, 0);
+        m.addMessagePart(data2, &ok);
+        stream << (ok ? "Ok" : "Error") << endl;
+
+        m.validateMessage();
+        stream << (int) m.getState() << endl;
+        stream << m.getLength() << endl;
+
+        p1.addMessage(m, &ok);
+        stream << (ok ? "Ok" : "Error") << endl;
+
+        uint8_t buffin[] = {0x02, 0x00, 0x00, 0x00, 0x10, 0x11}; // 2 v little-endian a pak 2 bajty dat
+        for(uint8_t* p = buffin; p<buffin+sizeof(buffin); p++){
+            stream << hex << *p << " ";
+        }
+        stream << endl;
+
+        const TMessage * matchedMessage = p1.tryMatchResponse(buffin, sizeof(buffin));
+        if(matchedMessage != nullptr) {
+
+            uint8_t buffer[100];
+
+            stream << matchedMessage->getData(buffer, 100) << endl;
+
+            for(int i = 0; i < matchedMessage->getLength(); i++) {
+                stream << hex << (unsigned char)buffer[i] << " ";
+            }
+            stream << endl;
+
+        }else stream << "not found match" << endl;
+    }
+
+
+
+
+
 	return 0;
 }
