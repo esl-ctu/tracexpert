6d8b7be0354e1382c348eaa6e1fc4bfcc43862d0
Petr Socha <petr.socha@fit.cvut.cz>
2023-11-17 13:25:28 +0100

    cmake dir + temp version of picoscopesdk find

---
+++ cmake/FindPicoScopeSDK.cmake
@@ -0,0 +1,25 @@
+find_path(PICOSCOPESDK_INCLUDE_DIR
+    NAMES
+        ps6000Api.h
+    PATHS
+        ENV "PROGRAMFILES"
+        $ENV{PROGRAMFILES}/Pico\ Technology/SDK
+        /opt/picoscope/
+    PATH_SUFFIXES
+        inc
+        include/
+        include/libps6000)
+
+find_library(PICOSCOPESDK_LIBRARY
+    NAMES ps6000
+    PATHS
+        ENV "PROGRAMFILES"
+        $ENV{PROGRAMFILES}/Pico\ Technology/SDK
+        /opt/picoscope/
+    PATH_SUFFIXES
+        lib)
+
+find_package_handle_standard_args(
+  PicoScopeSDK
+  FOUND_VAR PICOSCOPESDK_FOUND
+  REQUIRED_VARS PICOSCOPESDK_LIBRARY PICOSCOPESDK_INCLUDE_DIR)
