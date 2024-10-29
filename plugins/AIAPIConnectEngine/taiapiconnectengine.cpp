4a27507cf1491baf75ac7ba84cd20a5add09f4d5
Tom@ZbookG10 <tom@DESKTOP-2SPU364.localdomain>
2024-10-29 09:51:33 +0100

    AIAPI initial commit

---
+++ plugins/AIAPIConnectEngine/taiapiconnectengine.cpp
@@ -0,0 +1,3 @@
+#include "taiapiconnectengine.h"
+
+TAIAPIConnectEngine::TAIAPIConnectEngine() {}
---
+++ plugins/AIAPIConnectEngine/taiapiconnectengine.h
@@ -0,0 +1,12 @@
+#ifndef TAIAPICONNECTENGINE_H
+#define TAIAPICONNECTENGINE_H
+
+#include "AIAPIConnectEngine_global.h"
+
+class AIAPICONNECTENGINE_EXPORT TAIAPIConnectEngine
+{
+public:
+    TAIAPIConnectEngine();
+};
+
+#endif // TAIAPICONNECTENGINE_H
