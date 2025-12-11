80a1253ae7d7a6148fc0c87c36cbf30a66289ee7
Vojtěch Miškovský <miskovoj@fit.cvut.cz>
2025-12-11 15:02:35 +0100

    fix cmake searching for pandoc even if the proper path is provided

--- cmake/FindPandoc.cmake
+++ cmake/FindPandoc.cmake
@@ -1,25 +1,26 @@
-find_program(Pandoc_EXECUTABLE
-    NAMES pandoc pandoc.exe
-    HINTS
-        # Windows
-        ENV "PROGRAMFILES"
-        "$ENV{ProgramFiles}/Pandoc"
-        "$ENV{ProgramFiles\(x86\)}/Pandoc"
-        # Linux
-        /usr/bin
-        /usr/local/bin
-        /snap/bin
-        # MacOS
-        /opt/local/bin
-        /opt/homebrew/bin
-)
+if (NOT DEFINED PANDOC_EXEC)
+    find_program(Pandoc_EXECUTABLE
+        NAMES pandoc pandoc.exe
+        HINTS
+            # Windows
+            ENV "PROGRAMFILES"
+            "$ENV{ProgramFiles}/Pandoc"
+            "$ENV{ProgramFiles\(x86\)}/Pandoc"
+            # Linux
+            /usr/bin
+            /usr/local/bin
+            /snap/bin
+            # MacOS
+            /opt/local/bin
+            /opt/homebrew/bin
+    )
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Pandoc
-    REQUIRED_VARS Pandoc_EXECUTABLE
-)
+    include(FindPackageHandleStandardArgs)
+    find_package_handle_standard_args(Pandoc
+        REQUIRED_VARS Pandoc_EXECUTABLE
+    )
 
-if(Pandoc_FOUND)
-    set(PANDOC_EXEC "${Pandoc_EXECUTABLE}")
+    if(Pandoc_FOUND)
+        set(PANDOC_EXEC "${Pandoc_EXECUTABLE}")
+    endif()
 endif()
-
