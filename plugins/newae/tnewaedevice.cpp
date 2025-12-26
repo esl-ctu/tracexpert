867b14d3f768191cc1b22916a550e219a273fc22
Petr Socha <petr.socha@fit.cvut.cz>
2025-12-26 20:59:57 +0100

    copyright notice in source/header files

--- plugins/newae/tnewaedevice.cpp
+++ plugins/newae/tnewaedevice.cpp
@@ -1,3 +1,22 @@
+// TraceXpert
+// Copyright (C) 2025 Embedded Security Lab, CTU in Prague, and contributors.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// Contributors to this file:
+// Tomáš Přeučil (initial author)
+
 #include "tnewaedevice.h"
 
 TnewaeDevice::TnewaeDevice(const QString & name_in, const QString & sn_in, TNewae * plugin_in, targetType type_in, bool createdManually_in/* = true*/){
--- plugins/ps6000/tps6000scope.cpp
+++ plugins/ps6000/tps6000scope.cpp
@@ -1,3 +1,22 @@
+// TraceXpert
+// Copyright (C) 2025 Embedded Security Lab, CTU in Prague, and contributors.
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// 
+// Contributors to this file:
+// Petr Socha (initial author)
+
 #include "tps6000scope.h"
 
 TPS6000Scope::TPS6000Scope(const QString & name, const QString & info): m_name(name), m_info(info), m_initialized(false), m_running(false), m_handle(0), m_preTrigSamples(0), m_postTrigSamples(0), m_captures(0), m_timebase(0), m_samplingPeriod(0), m_channelEnabled(false) {
